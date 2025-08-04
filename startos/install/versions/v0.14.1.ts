577e6abd97d87fae8af5524cfc6a4b745dd6f913
arkanoider <github.913zc@simplelogin.com>
2025-08-04 18:55:22 +0200

    chore: fix a text comment in first alpha

--- startos/install/versions/v0.14.1.ts
+++ startos/install/versions/v0.14.1.ts
@@ -2,6 +2,6 @@ import { VersionInfo } from '@start9labs/start-sdk'
 
 export const v_0_14_1 = VersionInfo.of({
   version: '0.14.1:0',
-  releaseNotes: 'Initial release for StartOS 0.4.0',
+  releaseNotes: 'Initial release of Mostro daemon for StartOS 0.4.0',
   migrations: {},
 } as any)
